5cdf6232a1fb1cc447280fe29e3c63a608e8c838
caetanovns <caetanov120@gmail.com>
2024-08-22 19:19:15 -0300

    - added music player

--- songs/views.py
+++ songs/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 
 from songs.models import Song, Artist
+from numpy import random
 
 
 # Create your views here.
 def index(request):
-    random_songs = Song.objects.order_by('?').all()[:5]
+    random.seed(6)
+    min_id = Song.objects.order_by('id').first().id
+    max_id = Song.objects.order_by('id').last().id
+    music_random_ids = random.randint(min_id, max_id, size=7)
+    random_songs = Song.objects.filter(id__in=music_random_ids).all()[:7]
     most_populars = Song.objects.order_by('-artist__followers').all()[:30]
     artists = Artist.objects.order_by('-popularity').all()[:30]
 
@@ -14,5 +19,14 @@ def index(request):
 
 
 def show(request, pk):
+    random.seed(6)
+    min_id = Song.objects.order_by('id').first().id
+    max_id = Song.objects.order_by('id').last().id
+    music_random_ids = random.randint(min_id, max_id, size=7)
+    random_songs = Song.objects.filter(id__in=music_random_ids).all()[:7]
+    most_populars = Song.objects.order_by('-artist__followers').all()[:30]
+    artists = Artist.objects.order_by('-popularity').all()[:30]
+
     song = get_object_or_404(Song, pk=pk)
-    return render(request, 'songs/view.html', {'song': song})
+    return render(request, 'songs/view.html',
+                  {'random_songs': random_songs, 'songs': most_populars, 'artists': artists, 'song': song})
