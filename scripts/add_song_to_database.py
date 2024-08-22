f750fb477273f042bcfee4d1793234a4a263ef8f
caetanovns <caetanov120@gmail.com>
2024-08-22 14:41:07 -0300

    - updated script data

--- scripts/add_song_to_database.py
+++ scripts/add_song_to_database.py
@@ -6,6 +6,6 @@ with open('song_list', 'r') as file:
         artist = line.strip().split("/")[0]
         title = line.strip().split("/")[1]
         file_path = 'media/songs/' + artist + "/" + title
-        song = Song(artist=artist, title=title.split(".mp3")[0], file_path=file_path)
+        song = Song(artist=artist.replace('-', ' '), title=title.split(".mp3")[0].replace('-', ' '), file_path=file_path)
         song.save()
-        print(title)
+        #print(title.split(".mp3")[0].replace('-', ' '))
