07f01eefbecf88c5ed4ec5d396bb618cdc0d8149
caetanovns <caetanov120@gmail.com>
2024-08-22 19:34:16 -0300

    - updated for black template

--- songs/views.py
+++ songs/views.py
@@ -9,8 +9,8 @@ def index(request):
     random.seed(6)
     min_id = Song.objects.order_by('id').first().id
     max_id = Song.objects.order_by('id').last().id
-    music_random_ids = random.randint(min_id, max_id, size=7)
-    random_songs = Song.objects.filter(id__in=music_random_ids).all()[:7]
+    music_random_ids = random.randint(min_id, max_id, size=25)
+    random_songs = Song.objects.filter(id__in=music_random_ids).all()[:25]
     most_populars = Song.objects.order_by('-artist__followers').all()[:30]
     artists = Artist.objects.order_by('-popularity').all()[:30]
 
@@ -22,8 +22,8 @@ def show(request, pk):
     random.seed(6)
     min_id = Song.objects.order_by('id').first().id
     max_id = Song.objects.order_by('id').last().id
-    music_random_ids = random.randint(min_id, max_id, size=7)
-    random_songs = Song.objects.filter(id__in=music_random_ids).all()[:7]
+    music_random_ids = random.randint(min_id, max_id, size=25)
+    random_songs = Song.objects.filter(id__in=music_random_ids).all()[:25]
     most_populars = Song.objects.order_by('-artist__followers').all()[:30]
     artists = Artist.objects.order_by('-popularity').all()[:30]
 
