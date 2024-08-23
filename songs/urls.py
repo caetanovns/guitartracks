032745c1da9382f4e55c44ff07f1adba3cfed252
caetanovns <caetanov120@gmail.com>
2024-08-23 13:14:01 -0300

    - updated view and home template

--- songs/urls.py
+++ songs/urls.py
@@ -7,5 +7,5 @@ from songs import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('view/<int:pk>', views.show, name='view_song'),
+    path('<int:pk>', views.index, name='index'),
 ]
--- songs/views.py
+++ songs/views.py
@@ -2,31 +2,30 @@ from django.shortcuts import render, get_object_or_404
 
 from songs.models import Song, Artist
 from numpy import random
+from datetime import date
 
 
-# Create your views here.
-def index(request):
-    random.seed(6)
-    min_id = Song.objects.order_by('id').first().id
-    max_id = Song.objects.order_by('id').last().id
-    music_random_ids = random.randint(min_id, max_id, size=25)
-    random_songs = Song.objects.filter(id__in=music_random_ids).all()[:25]
-    most_populars = Song.objects.order_by('-artist__followers').all()[:30]
-    artists = Artist.objects.order_by('-popularity').all()[:30]
+def index(request, pk=None):
+    song = None
+    if pk:
+        song = get_object_or_404(Song, pk=pk)
 
+    data = get_music_home()
     return render(request, 'songs/index.html',
-                  {'random_songs': random_songs, 'songs': most_populars, 'artists': artists})
+                  {'random_songs': data[0], 'songs': data[1], 'artists': data[2], 'song': song})
 
 
-def show(request, pk):
-    random.seed(6)
+def get_music_home():
+    random.seed(int(date.today().day + date.today().month))
+
     min_id = Song.objects.order_by('id').first().id
     max_id = Song.objects.order_by('id').last().id
-    music_random_ids = random.randint(min_id, max_id, size=25)
-    random_songs = Song.objects.filter(id__in=music_random_ids).all()[:25]
-    most_populars = Song.objects.order_by('-artist__followers').all()[:30]
-    artists = Artist.objects.order_by('-popularity').all()[:30]
 
-    song = get_object_or_404(Song, pk=pk)
-    return render(request, 'songs/view.html',
-                  {'random_songs': random_songs, 'songs': most_populars, 'artists': artists, 'song': song})
+    music_random_ids = random.randint(min_id, max_id, size=25)
+    artist_ids = random.randint(26, 1300, size=30)
+
+    today_songs = Song.objects.filter(id__in=music_random_ids).all()[:15]
+    most_populars = Song.objects.order_by('-artist__followers').all()[:30]
+    artists = Artist.objects.filter(id__in=artist_ids).all()[:30]
+
+    return [today_songs, most_populars, artists]
